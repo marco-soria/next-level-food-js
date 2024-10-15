8db745c3831642b2df5cbea619db0f663822fcc9
marco-soria <marco.soriaa7@gmail.com>
2024-10-15 11:09:49 -0500

    full project

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,17 @@
 /** @type {import('next').NextConfig} */
 
-
-const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'maxschwarzmueller-nextjs-demo-users-image.s3.amazonaws.com',
-          port: '',
-          pathname: '/**',
-        },
-      ],
-    },
-  };
-
+const nextConfig = {};
+// const nextConfig = {
+//     images: {
+//       remotePatterns: [
+//         {
+//           protocol: 'https',
+//           hostname: 'maxschwarzmueller-nextjs-demo-users-image.s3.amazonaws.com',
+//           port: '',
+//           pathname: '/**',
+//         },
+//       ],
+//     },
+//   };
 
 export default nextConfig;
